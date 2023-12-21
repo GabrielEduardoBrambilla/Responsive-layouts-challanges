ea9a81206c581ec075fad22e5473d61a7a795be1
Brambs <gabriel128.br@gmail.com>
2023-12-21 10:40:26 -0300

    Instructions summary

---
+++ challange-1/INSTRUCTIONS.mk
@@ -0,0 +1,16 @@
+/* Problem solved
+ *
+ * 1) Limit the total width of
+ *    the .intro-conent to about half
+ *    of it's parent
+ *
+ * 2) Stop the text from overflowing
+ *    out the bottom at small screen
+ *    widths
+ *
+ * 3) Center the content in desktop screen
+ *
+ * 4) Use min-height and max-height
+ * You may modify the HTML if needed
+ *
+ */
